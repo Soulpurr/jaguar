7c365b056b0a46149d727a7d9aa468c37b558e04
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2021-12-24 12:18:15 +0530

    hi

---
+++ src/Components/Navbar/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Homepage() {
+    return (
+        <div className='Homepage'>
+            <h1>Hello World</h1>
+            <p>Lorem ipsum, dolor sit amet</p>
+        </div>
+    )
+}
+
+export default Homepage
